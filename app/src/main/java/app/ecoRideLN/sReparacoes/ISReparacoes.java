1d8bd38e9228459bd8a850dbdf4933e884d7e69f
Duarte Escairo <a106936@alunos.uminho.pt>
2026-05-06 15:40:01 +0100

    Separacao SReparacoes

--- app/src/main/java/app/ecoRideLN/sReparacoes/ISReparacoes.java
+++ app/src/main/java/app/ecoRideLN/sReparacoes/ISReparacoes.java
@@ -6,15 +6,20 @@ public interface ISReparacoes {
 
      public Reparacao registarReparacao(String nomenclatura, String descricao, float preco);
 
+     public void atualizarReparacao(int id, String novaNomenclatura, String novaDescricao, float novoPreco, boolean novaDisponibilidade);
+
      public Reparacao obterReparacao(int id);
 
      public boolean existeReparacao(int id);
 
      public boolean removerReparacao(int id);
 
-     public List<Reparacao> obterTodasReparacoes();
+     public List<Reparacao> obterReparacoes();
+
+     // Utilitários
 
      public List<Reparacao> obterReparacoesDisponiveis();
 
-     public void atualizarReparacao(int id, String novaNomenclatura, String novaDescricao, float novoPreco, boolean novaDisponibilidade);
+     public void mudarDisponibilidade(int id, boolean novaDisponibilidade);
+
 }
--- app/src/main/java/app/ecoRideLN/sReparacoes/SReparacoesFacade.java
+++ app/src/main/java/app/ecoRideLN/sReparacoes/SReparacoesFacade.java
@@ -17,31 +17,6 @@ public class SReparacoesFacade implements ISReparacoes {
      }
 
      @Override
-     public boolean existeReparacao(int id) {
-          return reparacaoDAO.containsKey(id);
-     }
-
-     @Override
-     public Reparacao obterReparacao(int id) {
-          return reparacaoDAO.get(id);
-     }
-
-     @Override
-     public boolean removerReparacao(int id) {
-          return reparacaoDAO.remove(id) != null;
-     }
-
-     @Override
-     public List<Reparacao> obterTodasReparacoes() {
-          return reparacaoDAO.values().stream().toList();
-     }
-
-     @Override
-     public List<Reparacao> obterReparacoesDisponiveis() {
-          return reparacaoDAO.values().stream().filter(Reparacao::isDisponivel).toList();
-     }
-
-     @Override
      public void atualizarReparacao(int id, String novaNomenclatura, String novaDescricao, float novoPreco, boolean novaDisponibilidade) {
           Reparacao reparacao = reparacaoDAO.get(id);
           if (reparacao != null) {
@@ -54,4 +29,43 @@ public class SReparacoesFacade implements ISReparacoes {
                throw new EcoRideException("Reparação com ID " + id + " não encontrada.");
           }
      }
+
+     @Override
+     public Reparacao obterReparacao(int id) {
+          return reparacaoDAO.get(id);
+     }
+
+     @Override
+     public boolean existeReparacao(int id) {
+          return reparacaoDAO.containsKey(id);
+     }
+
+     @Override
+     public boolean removerReparacao(int id) {
+          return reparacaoDAO.remove(id) != null;
+     }
+
+     @Override
+     public List<Reparacao> obterReparacoes() {
+          return reparacaoDAO.values().stream().toList();
+     }
+
+     // Utilitários
+
+     @Override
+     public List<Reparacao> obterReparacoesDisponiveis() {
+          return reparacaoDAO.values().stream().filter(Reparacao::isDisponivel).toList();
+     }
+
+     @Override
+     public void mudarDisponibilidade(int id, boolean novaDisponibilidade) {
+          Reparacao reparacao = reparacaoDAO.get(id);
+          if (reparacao != null) {
+               reparacao.setDisponivel(novaDisponibilidade);
+               reparacaoDAO.put(id, reparacao);
+          } else {
+               throw new EcoRideException("Reparação com ID " + id + " não encontrada");
+          }
+     }
+          
 }
